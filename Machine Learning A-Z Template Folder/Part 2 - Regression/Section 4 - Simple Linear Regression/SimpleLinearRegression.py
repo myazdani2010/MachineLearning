3592aac684c5e6326e06138b31b016829370dc49
Mohammad Yazdani <m.yazdani2010@gmail.com>
2018-10-17 15:35:57 -0700

    MLCourse/part2: visualize the training and test set to see how well the linear regression is plotting the line

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/SimpleLinearRegression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/SimpleLinearRegression.py
@@ -76,18 +76,32 @@ regressor.fit(X_train, y_train) # train the machine with data
 ##### Predicting the Test set result #####
 #------------------------------------------------------
 # here we use the test dataset to see how our trained machine is predecting the result
+
 y_pred = regressor.predict(X_test) 
 
 
 
 
 
+#------------------------------------------------------
+##### Visualizing the Train and Test set results #####
+#------------------------------------------------------
 
+# Plt the Train set
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
-
-
-
-
+# Plot the Test set
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
 
 
