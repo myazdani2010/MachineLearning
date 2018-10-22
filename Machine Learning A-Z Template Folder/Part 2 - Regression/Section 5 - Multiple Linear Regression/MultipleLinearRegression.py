9e2cc25f5c4672fa6118b5c2eba6212a37a2ec0d
Mohammad Yazdani <m.yazdani2010@gmail.com>
2018-10-21 20:11:12 -0700

    MLCourse/part2: Multiple linear Regression, deal with categorial data

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/MultipleLinearRegression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/MultipleLinearRegression.py
@@ -27,3 +27,25 @@ y = dataset.iloc[:, 4]
 
 
 
+
+#------------------------------------------
+##### 3- Categorical Data  #####
+#------------------------------------------
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelEncoder_X = LabelEncoder()
+
+# encode the State column
+X[:,3] = labelEncoder_X.fit_transform(X[:,3])
+
+# for categorial variables we must use Dummy Encoding 
+# make State column as categorical feature 
+oneHotEncoder = OneHotEncoder(categorical_features=[3])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+# now we should have the State column converted to 3 new numerical columns which contains 0s and 1s
+
+# Avoid Dummy Variable trap by removing any of the 3 new created columns from the Categorial column State
+ X = X[:, 1:]
+
