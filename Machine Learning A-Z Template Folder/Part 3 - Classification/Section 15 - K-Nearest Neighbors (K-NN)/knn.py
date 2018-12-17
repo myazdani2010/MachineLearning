9f7be4f6ba821c9c35440059faf3f20f55d51c02
Mohammad Yazdani <m.yazdani2010@gmail.com>
2018-12-17 14:49:43 -0800

    MLCourse/Part3: Fit K-NN to training and predict the result

--- Machine Learning A-Z Template Folder/Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/knn.py
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/knn.py
@@ -5,14 +5,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -20,15 +23,22 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+
 # Fitting K-NN to the Training set
-# Create your classifier here
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
+
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
+
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+# As we can the predections are much acurate that Logistic Regresssion
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
@@ -47,6 +57,7 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
+
 
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
