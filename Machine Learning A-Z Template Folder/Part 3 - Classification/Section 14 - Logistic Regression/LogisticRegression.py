e1c6b3b7cfc1123a97b42f49346c547b65459c74
Mohammad Yazdani <m.yazdani2010@gmail.com>
2018-12-17 13:00:20 -0800

    MLCourse/Part3: introduce confusion matrix to see the true and false predictions

--- Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/LogisticRegression.py
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/LogisticRegression.py
@@ -40,3 +40,8 @@ classifier.fit(X_train, y_train)
 
 # Predicting the Test set result
 y_pred = classifier.predict(X_test)
+
+
+# making the Confusion Matrix 
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
